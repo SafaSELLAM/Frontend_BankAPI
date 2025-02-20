8a95dd7829077333b8871fa72e012bc92b4e2b7e
SafaSELLAM <safa.sellam@outlook.fr>
2025-02-20 20:42:41 +0100

    Add Navigation with auth

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,19 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/argentBankLogo.png"
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../Store/authSlice.js";
 
 export const Navigation = () => {
+    const { isAuthenticated } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
 
+    const handleLogout = () => {
+        dispatch(logout())
+        navigate("/");
+    }
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
                 <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
-            <div>
-                <Link className="main-nav-item" to="/sign-in">
-                    <i className="fa fa-user-circle"></i> Sign In
-                </Link>
-            </div>
+            {isAuthenticated ? (
+                <div>
+                    <Link className="main-nav-item" to="/profile">
+                        <i className="fa fa-user-circle"></i>  Tony
+                    </Link>
+                    <button className="main-nav-item logout-button" onClick={handleLogout}>
+                        <i className="fa fa-sign-out"></i> Sign Out
+                    </button>
+
+                </div>
+
+            ) : (
+                <div>
+                    <Link className="main-nav-item" to="/sign-in">
+                        <i className="fa fa-user-circle"></i> Sign In
+                    </Link>
+                </div>
+            )}
+
         </nav>
     )
 }
